56ef2889e8ff0beb335d2f68c55969236ba569b9
Harish-b-03 <harishbalaji369@gmail.com>
2023-03-08 09:30:04 +0530

    Photos UPLOADED

--- src/data.ts
+++ src/data.ts
@@ -6,302 +6,714 @@ interface imageType {
 export const images:imageType[] = [
     {
         id: 1,
-        src: '/Images/Image-1.jpeg',
+        src: '/Images/Image-00001.jpeg',
     },
     {
         id: 2,
-        src: '/Images/Image-2.jpeg',
+        src: '/Images/Image-00002.jpeg',
     },
     {
         id: 3,
-        src: '/Images/Image-3.jpeg',
+        src: '/Images/Image-00003.jpeg',
     },
     {
         id: 4,
-        src: '/Images/Image-4.jpeg',
+        src: '/Images/Image-00004.jpeg',
     },
     {
         id: 5,
-        src: '/Images/Image-5.jpeg',
+        src: '/Images/Image-00005.jpeg',
     },
     {
         id: 6,
-        src: '/Images/Image-6.jpeg',
+        src: '/Images/Image-00006.jpeg',
     },
     {
         id: 7,
-        src: '/Images/Image-7.jpeg',
+        src: '/Images/Image-00007.jpeg',
     },
     {
         id: 8,
-        src: '/Images/Image-8.jpeg',
+        src: '/Images/Image-00008.jpeg',
     },
     {
         id: 9,
-        src: '/Images/Image-9.jpeg',
+        src: '/Images/Image-00009.jpeg',
     },
     {
         id: 10,
-        src: '/Images/Image-10.jpeg',
+        src: '/Images/Image-00010.jpeg',
     },
     {
         id: 11,
-        src: '/Images/Image-11.jpeg',
+        src: '/Images/Image-00011.jpeg',
     },
     {
         id: 12,
-        src: '/Images/Image-12.jpeg',
+        src: '/Images/Image-00012.jpeg',
     },
     {
         id: 13,
-        src: '/Images/Image-13.jpeg',
+        src: '/Images/Image-00013.jpeg',
     },
     {
         id: 14,
-        src: '/Images/Image-14.jpeg',
+        src: '/Images/Image-00014.jpeg',
     },
     {
         id: 15,
-        src: '/Images/Image-15.jpeg',
+        src: '/Images/Image-00015.jpeg',
+    },
+    {
+        id: 16,
+        src: '/Images/Image-00016.jpeg',
+    },
+    {
+        id: 17,
+        src: '/Images/Image-00017.jpeg',
+    },
+    {
+        id: 18,
+        src: '/Images/Image-00018.jpeg',
+    },
+    {
+        id: 19,
+        src: '/Images/Image-00019.jpeg',
+    },
+    {
+        id: 20,
+        src: '/Images/Image-00020.jpeg',
+    },
+    {
+        id: 21,
+        src: '/Images/Image-00021.jpeg',
+    },
+    {
+        id: 22,
+        src: '/Images/Image-00022.jpeg',
+    },
+    {
+        id: 23,
+        src: '/Images/Image-00023.jpeg',
+    },
+    {
+        id: 24,
+        src: '/Images/Image-00024.jpeg',
+    },
+    {
+        id: 25,
+        src: '/Images/Image-00025.jpeg',
+    },
+    {
+        id: 26,
+        src: '/Images/Image-00026.jpeg',
+    },
+    {
+        id: 27,
+        src: '/Images/Image-00027.jpeg',
+    },
+    {
+        id: 28,
+        src: '/Images/Image-00028.jpeg',
+    },
+    {
+        id: 29,
+        src: '/Images/Image-00029.jpeg',
+    },
+    {
+        id: 30,
+        src: '/Images/Image-00030.jpeg',
+    },
+    {
+        id: 31,
+        src: '/Images/Image-00031.jpeg',
+    },
+    {
+        id: 32,
+        src: '/Images/Image-00032.jpeg',
+    },
+    {
+        id: 33,
+        src: '/Images/Image-00033.jpeg',
+    },
+    {
+        id: 34,
+        src: '/Images/Image-00034.jpeg',
+    },
+    {
+        id: 35,
+        src: '/Images/Image-00035.jpeg',
+    },
+    {
+        id: 36,
+        src: '/Images/Image-00036.jpeg',
+    },
+    {
+        id: 37,
+        src: '/Images/Image-00037.jpeg',
+    },
+    {
+        id: 38,
+        src: '/Images/Image-00038.jpeg',
+    },
+    {
+        id: 39,
+        src: '/Images/Image-00039.jpeg',
+    },
+    {
+        id: 40,
+        src: '/Images/Image-00040.jpeg',
+    },
+    {
+        id: 41,
+        src: '/Images/Image-00041.jpeg',
+    },
+    {
+        id: 42,
+        src: '/Images/Image-00042.jpeg',
+    },
+    {
+        id: 43,
+        src: '/Images/Image-00043.jpeg',
+    },
+    {
+        id: 44,
+        src: '/Images/Image-00044.jpeg',
+    },
+    {
+        id: 45,
+        src: '/Images/Image-00045.jpeg',
+    },
+    {
+        id: 46,
+        src: '/Images/Image-00046.jpeg',
+    },
+    {
+        id: 47,
+        src: '/Images/Image-00047.jpeg',
+    },
+    {
+        id: 48,
+        src: '/Images/Image-00048.jpeg',
+    },
+    {
+        id: 49,
+        src: '/Images/Image-00049.jpeg',
+    },
+    {
+        id: 50,
+        src: '/Images/Image-00050.jpeg',
+    },
+    {
+        id: 51,
+        src: '/Images/Image-00051.jpeg',
+    },
+    {
+        id: 52,
+        src: '/Images/Image-00052.jpeg',
+    },
+    {
+        id: 53,
+        src: '/Images/Image-00053.jpeg',
+    },
+    {
+        id: 54,
+        src: '/Images/Image-00054.jpeg',
+    },
+    {
+        id: 55,
+        src: '/Images/Image-00055.jpeg',
+    },
+    {
+        id: 56,
+        src: '/Images/Image-00056.jpeg',
+    },
+    {
+        id: 57,
+        src: '/Images/Image-00057.jpeg',
+    },
+    {
+        id: 58,
+        src: '/Images/Image-00058.jpeg',
+    },
+    {
+        id: 59,
+        src: '/Images/Image-00059.jpeg',
+    },
+    {
+        id: 60,
+        src: '/Images/Image-00060.jpeg',
+    },
+    {
+        id: 61,
+        src: '/Images/Image-00061.jpeg',
+    },
+    {
+        id: 62,
+        src: '/Images/Image-00062.jpeg',
+    },
+    {
+        id: 63,
+        src: '/Images/Image-00063.jpeg',
+    },
+    {
+        id: 64,
+        src: '/Images/Image-00064.jpeg',
+    },
+    {
+        id: 65,
+        src: '/Images/Image-00065.jpeg',
+    },
+    {
+        id: 66,
+        src: '/Images/Image-00066.jpeg',
+    },
+    {
+        id: 67,
+        src: '/Images/Image-00067.jpeg',
+    },
+    {
+        id: 68,
+        src: '/Images/Image-00068.jpeg',
+    },
+    {
+        id: 69,
+        src: '/Images/Image-00069.jpeg',
+    },
+    {
+        id: 70,
+        src: '/Images/Image-00070.jpeg',
+    },
+    {
+        id: 71,
+        src: '/Images/Image-00071.jpeg',
+    },
+    {
+        id: 72,
+        src: '/Images/Image-00072.jpeg',
+    },
+    {
+        id: 73,
+        src: '/Images/Image-00073.jpeg',
+    },
+    {
+        id: 74,
+        src: '/Images/Image-00074.jpeg',
+    },
+    {
+        id: 75,
+        src: '/Images/Image-00075.jpeg',
+    },
+    {
+        id: 76,
+        src: '/Images/Image-00076.jpeg',
+    },
+    {
+        id: 77,
+        src: '/Images/Image-00077.jpeg',
+    },
+    {
+        id: 78,
+        src: '/Images/Image-00078.jpeg',
+    },
+    {
+        id: 79,
+        src: '/Images/Image-00079.jpeg',
+    },
+    {
+        id: 80,
+        src: '/Images/Image-00080.jpeg',
+    },
+    {
+        id: 81,
+        src: '/Images/Image-00081.jpeg',
+    },
+    {
+        id: 82,
+        src: '/Images/Image-00082.jpeg',
+    },
+    {
+        id: 83,
+        src: '/Images/Image-00083.jpeg',
+    },
+    {
+        id: 84,
+        src: '/Images/Image-00084.jpeg',
+    },
+    {
+        id: 85,
+        src: '/Images/Image-00085.jpeg',
+    },
+    {
+        id: 86,
+        src: '/Images/Image-00086.jpeg',
+    },
+    {
+        id: 87,
+        src: '/Images/Image-00087.jpeg',
+    },
+    {
+        id: 88,
+        src: '/Images/Image-00088.jpeg',
+    },
+    {
+        id: 89,
+        src: '/Images/Image-00089.jpeg',
+    },
+    {
+        id: 90,
+        src: '/Images/Image-00090.jpeg',
+    },
+    {
+        id: 91,
+        src: '/Images/Image-00091.jpeg',
+    },
+    {
+        id: 92,
+        src: '/Images/Image-00092.jpeg',
+    },
+    {
+        id: 93,
+        src: '/Images/Image-00093.jpeg',
+    },
+    {
+        id: 94,
+        src: '/Images/Image-00094.jpeg',
+    },
+    {
+        id: 95,
+        src: '/Images/Image-00095.jpeg',
+    },
+    {
+        id: 96,
+        src: '/Images/Image-00096.jpeg',
+    },
+    {
+        id: 96,
+        src: '/Images/Image-00097.jpeg',
+    },
+    {
+        id: 3,
+        src: '/Images/Image-00098.jpeg',
+    },
+    {
+        id: 4,
+        src: '/Images/Image-00099.jpeg',
+    },
+    {
+        id: 5,
+        src: '/Images/Image-00100.jpeg',
+    },
+    {
+        id: 6,
+        src: '/Images/Image-00101.jpeg',
+    },
+    {
+        id: 7,
+        src: '/Images/Image-00102.jpeg',
+    },
+    {
+        id: 8,
+        src: '/Images/Image-00103.jpeg',
+    },
+    {
+        id: 9,
+        src: '/Images/Image-00104.jpeg',
+    },
+    {
+        id: 10,
+        src: '/Images/Image-00105.jpeg',
+    },
+    {
+        id: 11,
+        src: '/Images/Image-00106.jpeg',
+    },
+    {
+        id: 12,
+        src: '/Images/Image-00107.jpeg',
+    },
+    {
+        id: 13,
+        src: '/Images/Image-00108.jpeg',
+    },
+    {
+        id: 14,
+        src: '/Images/Image-00109.jpeg',
+    },
+    {
+        id: 10,
+        src: '/Images/Image-00110.jpeg',
+    },
+    {
+        id: 11,
+        src: '/Images/Image-00111.jpeg',
+    },
+    {
+        id: 12,
+        src: '/Images/Image-00112.jpeg',
+    },
+    {
+        id: 13,
+        src: '/Images/Image-00113.jpeg',
+    },
+    {
+        id: 14,
+        src: '/Images/Image-00114.jpeg',
+    },
+    {
+        id: 15,
+        src: '/Images/Image-00115.jpeg',
     },
     {
         id: 1,
-        src: '/Images/Image-1.jpeg',
+        src: '/Images/Image-00116.jpeg',
     },
     {
         id: 2,
-        src: '/Images/Image-2.jpeg',
+        src: '/Images/Image-00117.jpeg',
     },
     {
         id: 3,
-        src: '/Images/Image-3.jpeg',
+        src: '/Images/Image-00118.jpeg',
     },
     {
         id: 4,
-        src: '/Images/Image-4.jpeg',
+        src: '/Images/Image-00119.jpeg',
     },
     {
         id: 5,
-        src: '/Images/Image-5.jpeg',
+        src: '/Images/Image-00120.jpeg',
     },
     {
         id: 6,
-        src: '/Images/Image-6.jpeg',
+        src: '/Images/Image-00121.jpeg',
     },
     {
         id: 7,
-        src: '/Images/Image-7.jpeg',
+        src: '/Images/Image-00122.jpeg',
     },
     {
         id: 8,
-        src: '/Images/Image-8.jpeg',
+        src: '/Images/Image-00123.jpeg',
     },
     {
         id: 9,
-        src: '/Images/Image-9.jpeg',
+        src: '/Images/Image-00124.jpeg',
     },
     {
         id: 10,
-        src: '/Images/Image-10.jpeg',
+        src: '/Images/Image-00125.jpeg',
     },
     {
         id: 11,
-        src: '/Images/Image-11.jpeg',
+        src: '/Images/Image-00126.jpeg',
     },
     {
         id: 12,
-        src: '/Images/Image-12.jpeg',
+        src: '/Images/Image-00127.jpeg',
     },
     {
         id: 13,
-        src: '/Images/Image-13.jpeg',
+        src: '/Images/Image-00128.jpeg',
     },
     {
         id: 14,
-        src: '/Images/Image-14.jpeg',
+        src: '/Images/Image-00129.jpeg',
     },
     {
         id: 15,
-        src: '/Images/Image-15.jpeg',
+        src: '/Images/Image-00130.jpeg',
     },
     {
         id: 1,
-        src: '/Images/Image-1.jpeg',
+        src: '/Images/Image-00131.jpeg',
     },
     {
         id: 2,
-        src: '/Images/Image-2.jpeg',
+        src: '/Images/Image-00132.jpeg',
     },
     {
         id: 3,
-        src: '/Images/Image-3.jpeg',
+        src: '/Images/Image-00133.jpeg',
     },
     {
         id: 4,
-        src: '/Images/Image-4.jpeg',
+        src: '/Images/Image-00134.jpeg',
     },
     {
         id: 5,
-        src: '/Images/Image-5.jpeg',
+        src: '/Images/Image-00135.jpeg',
     },
     {
         id: 6,
-        src: '/Images/Image-6.jpeg',
+        src: '/Images/Image-00136.jpeg',
     },
     {
         id: 7,
-        src: '/Images/Image-7.jpeg',
+        src: '/Images/Image-00137.jpeg',
     },
     {
         id: 8,
-        src: '/Images/Image-8.jpeg',
+        src: '/Images/Image-00138.jpeg',
     },
     {
         id: 9,
-        src: '/Images/Image-9.jpeg',
+        src: '/Images/Image-00139.jpeg',
     },
     {
         id: 10,
-        src: '/Images/Image-10.jpeg',
+        src: '/Images/Image-00140.jpeg',
     },
     {
         id: 11,
-        src: '/Images/Image-11.jpeg',
+        src: '/Images/Image-00141.jpeg',
     },
     {
         id: 12,
-        src: '/Images/Image-12.jpeg',
+        src: '/Images/Image-00142.jpeg',
     },
     {
         id: 13,
-        src: '/Images/Image-13.jpeg',
+        src: '/Images/Image-00143.jpeg',
     },
     {
         id: 14,
-        src: '/Images/Image-14.jpeg',
+        src: '/Images/Image-00144.jpeg',
     },
     {
         id: 15,
-        src: '/Images/Image-15.jpeg',
+        src: '/Images/Image-00145.jpeg',
     },
     {
         id: 1,
-        src: '/Images/Image-1.jpeg',
+        src: '/Images/Image-00146.jpeg',
     },
     {
         id: 2,
-        src: '/Images/Image-2.jpeg',
+        src: '/Images/Image-00147.jpeg',
     },
     {
         id: 3,
-        src: '/Images/Image-3.jpeg',
+        src: '/Images/Image-00148.jpeg',
     },
     {
         id: 4,
-        src: '/Images/Image-4.jpeg',
+        src: '/Images/Image-00149.jpeg',
     },
     {
         id: 5,
-        src: '/Images/Image-5.jpeg',
+        src: '/Images/Image-00150.jpeg',
     },
     {
         id: 6,
-        src: '/Images/Image-6.jpeg',
+        src: '/Images/Image-00151.jpeg',
     },
     {
         id: 7,
-        src: '/Images/Image-7.jpeg',
+        src: '/Images/Image-00152.jpeg',
     },
     {
         id: 8,
-        src: '/Images/Image-8.jpeg',
+        src: '/Images/Image-00153.jpeg',
     },
     {
         id: 9,
-        src: '/Images/Image-9.jpeg',
+        src: '/Images/Image-00154.jpeg',
     },
     {
         id: 10,
-        src: '/Images/Image-10.jpeg',
+        src: '/Images/Image-00155.jpeg',
     },
     {
         id: 11,
-        src: '/Images/Image-11.jpeg',
+        src: '/Images/Image-00156.jpeg',
     },
     {
         id: 12,
-        src: '/Images/Image-12.jpeg',
+        src: '/Images/Image-00157.jpeg',
     },
     {
         id: 13,
-        src: '/Images/Image-13.jpeg',
+        src: '/Images/Image-00158.jpeg',
     },
     {
         id: 14,
-        src: '/Images/Image-14.jpeg',
+        src: '/Images/Image-00159.jpeg',
     },
     {
         id: 15,
-        src: '/Images/Image-15.jpeg',
+        src: '/Images/Image-00160.jpeg',
     },
     {
         id: 1,
-        src: '/Images/Image-1.jpeg',
+        src: '/Images/Image-00161.jpeg',
     },
     {
         id: 2,
-        src: '/Images/Image-2.jpeg',
+        src: '/Images/Image-00162.jpeg',
     },
     {
         id: 3,
-        src: '/Images/Image-3.jpeg',
+        src: '/Images/Image-00163.jpeg',
     },
     {
         id: 4,
-        src: '/Images/Image-4.jpeg',
+        src: '/Images/Image-00164.jpeg',
     },
     {
         id: 5,
-        src: '/Images/Image-5.jpeg',
+        src: '/Images/Image-00165.jpeg',
     },
     {
         id: 6,
-        src: '/Images/Image-6.jpeg',
+        src: '/Images/Image-00166.jpeg',
     },
     {
         id: 7,
-        src: '/Images/Image-7.jpeg',
+        src: '/Images/Image-00167.jpeg',
     },
     {
         id: 8,
-        src: '/Images/Image-8.jpeg',
+        src: '/Images/Image-00168.jpeg',
     },
     {
         id: 9,
-        src: '/Images/Image-9.jpeg',
+        src: '/Images/Image-00169.jpeg',
     },
     {
         id: 10,
-        src: '/Images/Image-10.jpeg',
+        src: '/Images/Image-00170.jpeg',
     },
     {
         id: 11,
-        src: '/Images/Image-11.jpeg',
+        src: '/Images/Image-00171.jpeg',
     },
     {
         id: 12,
-        src: '/Images/Image-12.jpeg',
+        src: '/Images/Image-00172.jpeg',
     },
     {
         id: 13,
-        src: '/Images/Image-13.jpeg',
+        src: '/Images/Image-00173.jpeg',
     },
     {
         id: 14,
-        src: '/Images/Image-14.jpeg',
+        src: '/Images/Image-00174.jpeg',
     },
     {
         id: 15,
-        src: '/Images/Image-15.jpeg',
+        src: '/Images/Image-00175.jpeg',
+    },
+    {
+        id: 1,
+        src: '/Images/Image-00176.jpeg',
+    },
+    {
+        id: 2,
+        src: '/Images/Image-00177.jpeg',
+    },
+    {
+        id: 3,
+        src: '/Images/Image-00178.jpeg',
     },
 ]
